5c5df960bbac22f39e27e1f1a2452f171acd98cf
Rakibul Hasan Joy <rhj281@gmail.com>
2022-09-14 21:44:22 +0600

    Flutter chat app

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'login.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+    apiKey: "AIzaSyBdAyYCFWBfeeiypV62ZtduNj-ztkjJlOQ",
+    appId: "1:801866102436:web:e189cf122bd3c28cb69831",
+    messagingSenderId: "801866102436",
+    projectId: "chat-app-6a932",
+  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'chat',
+      theme: ThemeData(
+        primaryColor: Colors.orange[900],
+      ),
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
